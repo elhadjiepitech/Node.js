a7b954d2b1ffc7daa556b0d31f242245ea986d6c
El-Hadji Boubacar Traore <elhadjiepitech>
2024-02-17 13:24:13 +0100

    update

--- 1-les_bases/source_nodejs/main.js
+++
@@ -1,8 +0,0 @@
-// Node fonctionne comme javascript, mais avec des modules et des fonctions supplémentaires.
-
-
-console.log("Hello World!");
-// Pour compiler ce code et l'exécuter, vous devez ouvrir un terminal et exécuter la commande suivante:
-// node main.js
-
-
